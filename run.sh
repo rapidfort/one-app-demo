1d91a937ce04da378976a93a1c77791a6f9ed3a7
Vinod Gupta <vinod@rapidfort.com>
2023-02-16 17:13:13 -0800

    fix run-app

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-docker run -t -p 3000:3000 -p 3001:3001 -p 3002:3002 -p 3005:3005 -p 9229:9229 -e NODE_ENV=development   -v${PWD}/static:/opt/one-app/static oneamex/one-app-dev:5.0.0-rfhardened /bin/sh -c "node lib/server/index.js  --root-module-name=rf-oneapp-demo "
+APP=hello-oneapp
+docker run -t -p 3000:3000 -p 3001:3001 -p 3002:3002 -p 3005:3005 -p 9229:9229 -e NODE_ENV=development   -v${PWD}/static:/opt/one-app/static oneamex/one-app-dev:5.0.0-rfhardened /bin/sh -c "node lib/server/index.js  --root-module-name=${APP} "
